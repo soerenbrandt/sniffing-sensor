2d16f08e9715e32c72e6075c12b9afc49392e2e0
Soeren Brandt <36706786+soerenbrandt@users.noreply.github.com>
2023-01-31 18:50:48 -0800

    Soeren/adding new examples (#3)
    * Adding examples
    
    * Partial refactor and added figures

--- data/experiment_parameters.py
+++ data/experiment_parameters.py
@@ -1,112 +1,98 @@
+import sqlite3
 from abc import ABC
 from collections import namedtuple
+from enum import Enum
 from pathlib import Path
-import sqlite3
-from typing import Union, Tuple, List
+from typing import List, Tuple, Union
 
 import pandas as pd
 
-ANALYTES_DB = Path(__file__).parent.joinpath('Analytes.db')
-LIBRARY_DB = Path(__file__).parent.joinpath('Library.db')
+ANALYTES_DB = Path(__file__).parent.joinpath("Analytes.db")
+LIBRARY_DB = Path(__file__).parent.joinpath("Library.db")
 
-CHAMBER_IDS = {
-    'short': [4, 6, 9, 15],
-    'medium': [10, 11, 14],
-    'tall': [1, 2, 5, 7, 8, 12, 13, 16, 20, 26],
-    'other': [
-        3, 17, 18, 19, 21, 22, 23, 24
-    ]  # 3 is a glass vial, 17,18,19 are Teflon (narrow, med, wide), 21 is Talcon, 22,23,24 Teflon
-}
-
-SENSOR_IDS = {
-    'SM30_Sensor': [1, 6, 10, 11, 16, 18, 19, 20, 21, 23],
-    'TM40_Sensor': [12, 13, 14, 22],
-    'Hybrid_Sensor': [15, 17],
-    'Ida_Sensor': [2, 3, 4, 5],
-    'func_Sensor': [7, 8, 9]
-}
 
 EXPERIMENTAL_MEASUREMENTS = [
-    'Vapor Pressure', 'Boiling Point', 'Flash Point', 'Viscosity'
+    "Vapor Pressure",
+    "Boiling Point",
+    "Flash Point",
+    "Viscosity",
 ]
+
+
+class Chamber(Enum):
+    SHORT = [4, 6, 9, 15]
+    MEDIUM = [10, 11, 14]
+    TALL = [1, 2, 5, 7, 8, 12, 13, 16, 20, 26]
+
+    # Other:
+    #  3 is a glass vial,
+    #  17,18,19 are Teflon (narrow, med, wide),
+    #  21 is Talcon,
+    #  22,23,24 Teflon
+    OTHER = [3, 17, 18, 19, 21, 22, 23, 24]
+
+    @property
+    def sql_query(self):
+        return f"Chamber_ID IN ({', '.join(map(str, self.value))})"
+
+
+class Sensor(Enum):
+    SM30 = [1, 6, 10, 11, 16, 18, 19, 20, 21, 23]
+    TM40 = [12, 13, 14, 22]
+    HYBRID = [15, 17]
+    IDA = [2, 3, 4, 5]
+    FUNCTIONALIZED = [7, 8, 9]
+
+    @property
+    def sql_query(self):
+        return f"Sensor_ID IN ({', '.join(map(str, self.value))})"
 
 
 class ExperimentParameter(ABC):
     sql_label: str
-    value: list
+
+    def __init__(self, *values: float, value_range: List[float] = None) -> None:
+        if value_range and not len(value_range) == 2:
+            raise ValueError("value_range must contain two values.")
+        self.values = values
+        self.range = value_range
 
     @property
     def sql_query(self) -> str:
-        return f"{self.sql_label} IN ({', '.join(map(str, self.value))})"
-
-    def _sql_query_for_floats_and_ranges(self) -> str:
-        try:
-            return f"{self.sql_label} BETWEEN {str(self.value[0])} AND {str(self.value[1])}"
-        except (IndexError, TypeError):
-            return f"{self.sql_label} = {str(self.value)}"
-
-
-class Chamber(ExperimentParameter):
-    sql_label = "Chamber_ID"
-
-    def __init__(self, size: str) -> None:
-        if not size in CHAMBER_IDS:
-            raise ValueError(f"Invalid parameter: {size}")
-        self.value = CHAMBER_IDS[size]
-
-
-class Sensor(ExperimentParameter):
-    sql_label = "Sensor_ID"
-
-    def __init__(self, kind: str) -> None:
-        if not kind in SENSOR_IDS:
-            raise ValueError(f"Invalid parameter: {kind}")
-        self.value = SENSOR_IDS[kind]
+        query_parts = []
+        if self.values:
+            query_parts.append(
+                f"{self.sql_label} IN ({', '.join(map(str, self.values))})"
+            )
+        if self.range:
+            query_parts.append(
+                f"{self.sql_label} BETWEEN {str(self.range[0])} AND {str(self.range[1])}"
+            )
+        return " AND ".join(query_parts)
 
 
 class InjectionTime(ExperimentParameter):
     sql_label = "Injection_Time_s"
 
-    def __init__(self, time: Union[float, Tuple[float, float]]) -> None:
-        self.value = time
-
-    @property
-    def sql_query(self) -> str:
-        return self._sql_query_for_floats_and_ranges()
-
 
 class InjectionRate(ExperimentParameter):
     sql_label = "Injection_Rate_mL_per_min"
-
-    def __init__(self, rate: Union[float, Tuple[float, float]]) -> None:
-        self.value = rate
-
-    @property
-    def sql_query(self) -> str:
-        return self._sql_query_for_floats_and_ranges()
 
 
 class InjectionVolume(ExperimentParameter):
     sql_label = "Injection_Volume_mL"
 
-    def __init__(self, volume: Union[float, Tuple[float, float]]) -> None:
-        self.value = volume
-
-    @property
-    def sql_query(self) -> str:
-        return self._sql_query_for_floats_and_ranges()
-
 
 class Custom(ExperimentParameter):
-    def __init__(self, query: str) -> None:
-        self.value = query
-
-    def sql_query(self) -> str:
-        return self.value
+    def __init__(self, label: str, **kwargs) -> None:
+        self.sql_label = label
+        super().__init__(**kwargs)
 
 
 class ExperimentFilter(object):
-    def __init__(self, *parameters: ExperimentParameter) -> None:
+    def __init__(
+        self, *parameters: Union[ExperimentParameter, Chamber, Sensor]
+    ) -> None:
         self.conn = sqlite3.connect(LIBRARY_DB)
         self.parameters = parameters
 
@@ -118,7 +104,8 @@ class ExperimentFilter(object):
         ]
 
         sub_df = pd.read_sql_query(
-            ' AND '.join([query_base] + parameter_queries), self.conn)
+            " AND ".join([query_base] + parameter_queries), self.conn
+        )
 
         return list(set(items) & set(sub_df.Experiment_ID))
 
@@ -127,11 +114,13 @@ class DataFilter(object):
     """Removes compounds for which experimental data is not available.
 
     parameters: Vapor Pressure, Boiling Point, Flash Point, Viscosity"""
+
     def __init__(self, *parameters: str):
         for parameter in parameters:
             if parameter not in EXPERIMENTAL_MEASUREMENTS:
                 raise ValueError(
-                    f"Parameter must be in: {EXPERIMENTAL_MEASUREMENTS}")
+                    f"Parameter must be in: {EXPERIMENTAL_MEASUREMENTS}"
+                )
         self.conn = sqlite3.connect(ANALYTES_DB)
         self.parameters = parameters
 
@@ -142,13 +131,13 @@ class DataFilter(object):
                         (SELECT "Analyte ID" FROM AnalyteData
                         WHERE %s)
                     """
-        query = query % " AND ".join([
-            f"""'{parameter}' IS NOT null)""" for parameter in self.parameters
-        ])
+        query = query % " AND ".join(
+            [f"""'{parameter}' IS NOT null)""" for parameter in self.parameters]
+        )
 
         exp_id_df = pd.read_sql_query(query, self.conn)
 
-        return list(set(items) & set(exp_id_df['Experiment Number']))
+        return list(set(items) & set(exp_id_df["Experiment Number"]))
 
 
 def _create_experiment_label_df(experiments):
@@ -157,15 +146,18 @@ def _create_experiment_label_df(experiments):
     experiments_df = pd.read_sql_query(
         f"""SELECT "Experiment Number", "Analyte ID" FROM ExperimentData
             WHERE "Experiment Number" IN ({', '.join(map(str, experiments))})""",
-        conn)
+        conn,
+    )
 
     label_df = pd.read_sql_query(
         f"""SELECT * FROM AnalyteData WHERE "Analyte ID"
                 IN ({",".join(experiments_df["Analyte ID"].astype(str))})""",
-        conn)
+        conn,
+    )
 
     experiment_label_df = experiments_df.merge(
-        label_df, 'left', on='Analyte ID').set_index('Experiment Number')
+        label_df, "left", on="Analyte ID"
+    ).set_index("Experiment Number")
     return experiment_label_df
 
 
@@ -183,8 +175,9 @@ def create_physical_property_label(experiments, *properties: str):
         raise ValueError("Must pass at least one property")
     properties = [property.title() for property in properties]
 
-    Label = namedtuple("Properties",
-                       ["_".join(property.split()) for property in properties])
+    Label = namedtuple(
+        "Properties", ["_".join(property.split()) for property in properties]
+    )
 
     experiments_label_df = _create_experiment_label_df(experiments)
 
@@ -210,20 +203,29 @@ def create_concentration_label(experiments):
     for exp_id in experiments:
         components = experiments_label_df.loc[
             exp_id,
-            sorted([
-                column for column in experiments_label_df.columns
-                if 'Component' in column
-            ])].dropna()
+            sorted(
+                [
+                    column
+                    for column in experiments_label_df.columns
+                    if "Component" in column
+                ]
+            ),
+        ].dropna()
         concentrations = experiments_label_df.loc[
             exp_id,
-            sorted([
-                column for column in experiments_label_df.columns
-                if 'Concentration' in column
-            ])]
+            sorted(
+                [
+                    column
+                    for column in experiments_label_df.columns
+                    if "Concentration" in column
+                ]
+            ),
+        ]
 
         Label = namedtuple(
             "Concentration",
-            ["_".join(component.split()) for component in components])
-        labels[exp_id] = Label(*concentrations[:len(components)])
+            ["_".join(component.split()) for component in components],
+        )
+        labels[exp_id] = Label(*concentrations[: len(components)])
 
     return labels
--- data/plot_functions.py
+++ data/plot_functions.py
@@ -1,10 +1,60 @@
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from colour import Color
+from sklearn.metrics import r2_score
 
 LABELSIZE = 8
 LEGENDSIZE = 8
 TICKSIZE = 8
+
+
+def get_markers(n=None):
+    # gives a list of markers
+    markers = [
+        "o",
+        "v",
+        "^",
+        "<",
+        ">",
+        "s",
+        "p",
+        "P",
+        "*",
+        "X",
+        "D",
+        "d",
+        "8",
+        "H",
+        "h",
+        "o",
+        "v",
+        "^",
+        "<",
+        ">",
+    ]
+    if n is None:
+        return markers
+    else:
+        return markers[0:n]
+
+
+def get_gradient_colors(n, start, end):
+    # gives you a color range between start and end
+    if isinstance(start, str):
+        first_color = Color(start)
+    elif isinstance(start, tuple):
+        first_color = Color(rgb=start)
+
+    if isinstance(end, str):
+        final_color = Color(end)
+    elif isinstance(end, tuple):
+        final_color = Color(rgb=end)
+
+    colors = list(first_color.range_to(final_color, n))
+    colors = [color.rgb for color in colors]
+
+    return colors
 
 
 def plot_array_data(
@@ -90,3 +140,64 @@ def plot_array_data(
         ax.tick_params(labelsize=TICKSIZE)
     else:
         ax.set_xticks = []
+
+
+def plot_regression(
+    actual,
+    predicted,
+    c="k",
+    model=None,
+    initial_colorID=0,
+    ax=None,
+    labelsize: int = 12,
+    ticksize: int = 12,
+):
+    # plots the real vs predicted concentrations
+    if ax == None:
+        plt.figure(figsize=(3, 5))
+        ax = plt.gca()
+    ax.set_xlabel("Actual Pentane Concentration", fontsize=labelsize)
+    ax.set_ylabel("Predicted Pentane Concentration", fontsize=labelsize)
+    ax.tick_params(
+        direction="out",
+        top=False,
+        bottom=True,
+        left=True,
+        right=False,
+        labelsize=ticksize,
+    )
+
+    # plot line representing 1:1 correspondence
+    ax.plot(
+        np.linspace(0, 1, 100), np.linspace(0, 1, 100), "--", c="k", linewidth=1
+    )
+
+    # scatter predictions
+    ax.scatter(np.array(actual), np.array(predicted), c=c, s=15)
+
+    # Label performance
+    score = r2_score(actual, predicted)
+    ax.text(
+        0.065,
+        0.97,
+        "$R^2$ = " + str(round(score, 3)),
+        fontsize=labelsize,
+        verticalalignment="top",
+        transform=ax.transAxes,
+    )
+
+    # add model regions
+    if model:
+        for n, segment in enumerate(model["segments"]):
+            ax.axvspan(
+                segment.start_t,
+                segment.inclusive_end_t,
+                facecolor=segmentcolors[
+                    np.mod(n + initial_colorID, len(segmentcolors))
+                ],
+                alpha=0.1,
+            )
+
+    # clean up plot
+    ax.set_xlim([-0.05, 1.05])
+    ax.set_ylim([-0.05, 1.05])
